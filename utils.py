3686f4b604f736c79e775aa1e181089c2574c33d
alexandremalecot <malecot@apc.in2p3.fr>
2020-11-04 18:23:35 +0100

    Corrected infinite loop when typing a wrong integer

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 
 def get_integer_in_range(min_bound,max_bound,prompt_string=None):
-    n = input(prompt_string)
     n_int = 0
     while(n_int < min_bound or n_int > max_bound):
+        n = input(prompt_string)
         try:
             n_int = int(n)
         except ValueError:
